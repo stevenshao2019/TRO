25d1f6ce329a37939026ccb5136731a7384cc4d0
stevenshao2019 <55921671+stevenshao2019@users.noreply.github.com>
2024-09-12 10:53:39 +0800

    Update troinst.sh

--- troinst.sh
+++ troinst.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y curl
-sudo apt install certbot
+sudo apt install -y certbot
 curl -LO https://github.com/trojan-gfw/trojan/releases/download/v1.16.0/trojan-1.16.0-linux-amd64.tar.xz
 tar -xvJf trojan-1.16.0-linux-amd64.tar.xz
 sudo mv trojan /usr/local/bin/
