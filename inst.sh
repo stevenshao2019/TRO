75f942d964d9a6ff2e6b02a3ac7b1bdce955d65a
stevenshao2019 <55921671+stevenshao2019@users.noreply.github.com>
2025-05-11 19:38:16 +0800

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -4,7 +4,7 @@ wget -N --no-check-certificate  https://raw.githubusercontent.com/stevenshao2019
 wget -N --no-check-certificate  https://raw.githubusercontent.com/stevenshao2019/TRO/master/trojan.service
 wget -N --no-check-certificate  https://raw.githubusercontent.com/stevenshao2019/TRO/master/config.json
 chmod +x *.sh
-sleep 60
+sleep 20
 ./troinst.sh
 sudo apt-get install -y expect
 ./crtssl.sh
